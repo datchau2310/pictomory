bcf76b90ce41ed461ec49bde155412d3912ff083
Dat Chau <datchau23101994@gmail.com>
2019-10-22 17:18:45 +0700

    update code

--- app/js/slider.js
+++ app/js/slider.js
@@ -60,12 +60,50 @@ $('.slider_nav').on('click', '.slick-slide', function(event) {
 $('.multiple_items').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+        }
+        }, {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+        }
+        }, {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+        }
+    }]
 });
 
 // slider sub main
 $('.list_detail_gallery').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+        }
+        }, {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+        }
+        }, {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+        }
+    }]
 });
